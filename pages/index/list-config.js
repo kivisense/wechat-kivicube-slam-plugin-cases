f09c860ea3faafb16418d419ca17e873a19eec78
宏金 蓝 <honkinlan@163.com>
2023-01-31 19:37:29 +0800

    update: 新增入口处判断

---
+++ pages/index/list-config.js
@@ -0,0 +1,14 @@
+const list = [
+  {
+    name: "AR识别 - 世界AR放置",
+    thumb: "cloudar.png",
+    path: "/pages/sample/sample",
+  },
+  {
+    name: "AR识别 - 平面AR放置",
+    thumb: "cloudar.png",
+    path: "/pages/sample/sample",
+  },
+];
+
+export default list;
